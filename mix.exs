940db480a0de00f14ae87f2abafa17f36654f68b
Guarzo <guarzo.eve@gmail.com>
2025-06-13 08:43:29 -0400

    bump again?

--- mix.exs
+++ mix.exs
@@ -4,7 +4,7 @@ defmodule WandererKills.MixProject do
   def project do
     [
       app: :wanderer_kills,
-      version: "0.1.1",
+      version: "0.1.2",
       elixir: "~> 1.18",
       start_permanent: Mix.env() == :prod,
       deps: deps(),
